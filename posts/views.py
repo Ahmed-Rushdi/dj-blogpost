3a3620bce8904fa20cab451b46d9f580e24b1ab0
Ahmed El-Kilani <ahmedelkilani01@gmail.com>
2024-09-01 02:23:49 +0300

    finished posts app v0.1

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def posts(request):
+    return render(
+        request,
+        "posts/posts_list.html",
+        {
+            "posts": [
+                "Post 1",
+                "Post 2",
+                "Post 3",
+                "Post 4",
+                "Post 5",
+                "Post 6",
+                "Post 7",
+                "Post 8",
+                "Post 9",
+                "Post 10",
+            ]
+        },
+    )
