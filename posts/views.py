8eca6aceb720d30d6c246d063a5436bcb949bc85
Ahmed El-Kilani <ahmedelkilani01@gmail.com>
2024-09-03 01:30:18 +0300

    added single post page | refactores the code a little

--- posts/views.py
+++ posts/views.py
@@ -1,13 +1,24 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from .models import Post
 
 
 # Create your views here.
-def posts(request):
+def posts(request: HttpRequest) -> HttpResponse:
     return render(
         request,
         "posts/posts_list.html",
         {
             "posts": Post.objects.all().order_by("created_at").reverse(),
+        },
+    )
+
+
+def post_page(request: HttpRequest, slug: str) -> HttpResponse:
+    return render(
+        request,
+        "posts/post_page.html",
+        {
+            "post": Post.objects.get(slug=slug),
         },
     )
