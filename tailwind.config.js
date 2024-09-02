dd66d90a00534d38a883a1fae2e52bfe3c7c84e3
Ahmed El-Kilani <ahmedelkilani01@gmail.com>
2024-09-03 02:41:26 +0300

    migrating css to tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './**/templates/**/*.html', // Adjust this path to match your Django templates location
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
