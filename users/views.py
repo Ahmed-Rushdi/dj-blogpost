adafd1222a84f63eac5f6b99549ffb6a5b2a2415
Ahmed El-Kilani <ahmedelkilani01@gmail.com>
2024-09-03 04:34:50 +0300

    added User model to admin

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpRequest, HttpResponse
 # Create your views here.
 
 
-def register(request):
+def register(request: HttpRequest) -> HttpResponse:
     return render(request, "users/register.html")
