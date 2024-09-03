6dff19f0a4477a3406af67f8cf4d369dbb308f98
Ahmed El-Kilani <ahmedelkilani01@gmail.com>
2024-09-03 04:02:26 +0300

    added users app with placehlder regeistration page

---
+++ users/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+
+class User(models.Model):
+    username = models.CharField(max_length=255)
+    email = models.EmailField()
+    password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.username
