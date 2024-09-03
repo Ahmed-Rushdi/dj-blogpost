5e03ce9a7412c2f3fcf519c91fe981148f2d504b
Ahmed El-Kilani <ahmedelkilani01@gmail.com>
2024-09-03 05:49:37 +0300

    removed usermodel to use the builtin django users

--- users/views.py
+++ users/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+
 from django.http import HttpRequest, HttpResponse
+
 # Create your views here.
 
 
 def register(request: HttpRequest) -> HttpResponse:
-    return render(request, "users/register.html")
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("posts:list")
+        else:
+            print(form.errors)
+    else:
+        form = UserCreationForm()
+    return render(request, "users/register.html", {"form": UserCreationForm()})
